53ab8841857baa24d4029947454e44da585d23a1
Milosz Pietrzak <97665937+miloZs@users.noreply.github.com>
2022-05-17 10:30:42 +0200

    Update RynekInternet.sql

--- RynekInternet.sql
+++ RynekInternet.sql
@@ -4,8 +4,8 @@ CREATE TABLE Gry(
 	Tytul VARCHAR(50) NOT NULL PRIMARY KEY 
 )
 
-CREATE TABLE U¿ytkownicy(
-	Nazwa_U¿ytkownika VARCHAR(50) NOT NULL,
+CREATE TABLE Uzytkownicy(
+	Nazwa_Uzytkownika VARCHAR(50) NOT NULL,
 	Saldo MONEY,
 	Imie VARCHAR(50) NOT NULL CHECK(len(Imie) >= 3),
 	Nazwisko VARCHAR(50) NOT NULL CHECK(len(Nazwisko) >= 3),
@@ -15,14 +15,14 @@ CREATE TABLE U¿ytkownicy(
 
 CREATE TABLE Oferty(
 	Data_Wystawienia DATE NOT NULL,
-	Data_Wa¿noœci DATE NOT NULL,
-	Status VARCHAR(20) CHECK(Status LIKE 'Dostêpny' OR Status LIKE 'Nie Dostêpny')
+	Data_WaznoÅ“ci DATE NOT NULL,
+	Status VARCHAR(20) CHECK(Status LIKE 'DostÃªpny' OR Status LIKE 'Nie DostÃªpny')
 )
 
 CREATE TABLE Przedmioty(
 	Nazwa_Przedmiotu VARCHAR(50) NOT NULL CHECK(len(Nazwa_Przedmiotu) >= 5),
 	Id_Przedmiotu INT PRIMARY KEY,
-	Email_u¿ytkownika VARCHAR(50) NOT NULL REFERENCES U¿ytkownicy(Email),
+	Email_uzytkownika VARCHAR(50) NOT NULL REFERENCES Uzytkownicy(Email),
 	Gra VARCHAR(50) NOT NULL REFERENCES Gry(Tytul)
 )
 
